958df81bc34025ff71a227806f58c9ad3769f0ae
KBANKNET\benjamas.ch <benjamas.ch@kbtg.tech>
2021-09-07 12:15:01 +0700

    add EmployeeControllerFailureTest

--- src/test/java/com/example/demoapp/employees/EmployeeControllerTest.java
+++ src/test/java/com/example/demoapp/employees/EmployeeControllerTest.java
@@ -1,5 +1,7 @@
 package com.example.demoapp.employees;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,15 +15,25 @@ public class EmployeeControllerTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
-
     @Autowired
     private EmployeeRepository employeeRepository;
+
+//    @BeforeEach
+//    public void initialDataForTest() {
+//        Employee emp = new Employee();
+//        emp.setName("benjamas");
+//        employeeRepository.save(emp);
+//    }
+
+    @AfterEach
+    public void deleteDataForTest() {
+        employeeRepository.deleteAll();
+    }
 
     @Test
     public void listEmployee() {
         // Act
         EmployeeResponse[] results = restTemplate.getForObject("/employees", EmployeeResponse[].class);
-
         // Assert
         assertEquals(2, results.length);
         assertEquals(1, results[0].getId());
